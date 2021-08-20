90b053ec502968a78941edc6bfbf2999e99e2670
AndrewJ <andrew.james3@my.jcu.edu.au>
2021-08-20 10:47:37 +1000

    Refactor broken_score program as functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,15 +1,25 @@
 """
 Broken Score program from prac 3 CP1404
 """
+from random import randrange
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-elif score <= 100:
-    print("Excellent")
-else:
-    print("Invalid score")
+
+def main():
+    score = float(input("Enter score: "))
+    score_description = get_score_description(score)
+    print(score_description)
+
+
+def get_score_description(score):
+    if score < 0 or score > 100:
+        score_description = "Invalid score"
+    elif score < 50:
+        score_description = "Bad"
+    elif score < 90:
+        score_description = "Passable"
+    else:
+        score_description = "Excellent"
+    return score_description
+
+
+main()
