34e514c5a7057192a6d223c20d6fa839051fb5db
AndrewJ <andrew.james3@my.jcu.edu.au>
2021-08-27 13:49:33 +1000

    Complete subject_reader.py task

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,30 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def print_data(data):
+    max_len_subject = max(len(line[0]) for line in data)
+    max_len_lecturer = max(len(line[1]) for line in data)
+    max_len_number_of_students = max(len(str(line[2])) for line in data)
+    for line in data:
+        print(f'{line[0]:{max_len_subject}} is taught by '
+              f'{line[1]:{max_len_lecturer}} and has '
+              f'{line[2]:{max_len_number_of_students}} students')
 
 
 main()
