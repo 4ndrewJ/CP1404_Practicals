4ceaffc324b96d17c9886e2e92915e574973702d
AndrewJ <andrew.james3@my.jcu.edu.au>
2021-09-19 15:25:41 +1000

    Add completed code for stage 1 of convert_miles_km task

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -8,6 +8,9 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 
 
+MILE_TO_KM_FACTOR = 1.60934
+
+
 class ConvertMilesKm(App):
     """ Kivy App for converting miles to km """
     output_text = StringProperty()
@@ -17,6 +20,15 @@ class ConvertMilesKm(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         self.output_text = 'Temp Text'
         return self.root
+
+    def handle_increment(self, text_input, increment):
+        """
+        Takes text TextInput object and increments it's value by the increment
+        """
+        text_input.text = str(float(text_input.text) + increment)
+
+    def handle_calculate(self, text_input):
+        self.output_text = str(float(text_input.text) * MILE_TO_KM_FACTOR)
 
 
 ConvertMilesKm().run()
