0bd4839ece3b9e3193156caf22d73c410eab3813
AndrewJ <andrew.james3@my.jcu.edu.au>
2021-10-09 21:50:43 +1000

    Add completed code for sort_files_2.py task

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -3,12 +3,14 @@ import shutil
 
 
 os.chdir('FilesToSort')
-new_dirs = []
+ext_to_dir = {}
 for filename in os.listdir('.'):
     if os.path.isdir(filename):
         continue
     extension = filename.split('.')[-1]
-    if extension not in new_dirs:
-        os.mkdir(extension)
-        new_dirs.append(extension)
-    shutil.move(filename, extension + '/' + filename)
+    if extension not in ext_to_dir:
+        new_dir = input(f'What category would you like to sort {extension} files into? ')
+        if new_dir not in ext_to_dir.values():
+            os.mkdir(new_dir)
+        ext_to_dir[extension] = new_dir
+    shutil.move(filename, ext_to_dir[extension] + '/' + filename)
